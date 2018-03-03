fbbc87382d9f7796511c8a9f953b829a1af420d5
imran <imran@ackosoft.com>
2018-03-03 10:20:17 +0600

    Bootstrap framework is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {BsDropdownModule, ModalModule, TooltipModule} from "ngx-bootstrap";
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
